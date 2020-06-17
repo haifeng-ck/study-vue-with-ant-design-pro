2e6c203f2335b5b99bb5db6cfb55ae68ce4d5c85
junlong.zhou <2530324624@qq.com>
2020-06-17 15:35:17 +0800

    Single File Components

--- demo1-single-file-components/src/App.vue
+++ demo1-single-file-components/src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <todo-list>
+            <!-- 使用默认插槽，可以省略外层 `template` 标签 -->
+            <!--<template v-slot>-->
+            <todo-item
+                    v-for="item in list"
+                    :key="item.id"
+                    :title="item.title"
+                    :del="item.del"
+                    @delete="handleDelete"
+            >
+                <!-- 具名插槽 -->
+                <!-- Vue 2.5.x 插槽语法 -->
+                <img slot="pre-icon"
+                     src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1564264355,3357835124&fm=26&gp=0.jpg"
+                     alt="前置图标" style="width: 20px; height: 20px;">
+                <img slot="suf-icon"
+                     src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3745390473,3851381912&fm=26&gp=0.jpg"
+                     alt="后置图标" style="width: 20px; height: 20px;">
+                <!-- Vue 2.6.x 插槽语法 -->
+                <template v-slot:last-icon="{ value }">
+                    <img src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2586427976,2127777837&fm=26&gp=0.jpg"
+                         alt="前置图标" style="width: 20px; height: 20px;">
+                    <span>{{ value }}</span>
+                </template>
+            </todo-item>
+            <!--</template>-->
+        </todo-list>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import TodoList from "./components/TodoList"
+    import TodoItem from "./components/TodoItem"
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            TodoList,
+            TodoItem
+        },
+        data() {
+            return {
+                list: [
+                    {
+                        id: 1,
+                        title: '光绪',
+                        del: false
+                    },
+                    {
+                        id: 2,
+                        title: '慈禧',
+                        del: true
+                    },
+                    {
+                        id: 3,
+                        title: '荣中堂',
+                        del: false
+                    }
+                ]
+            }
+        },
+        methods: {
+            handleDelete(val) {
+                console.log('handleDelete', val)
+            }
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ demo1-single-file-components/src/components/TodoItem.vue
@@ -0,0 +1,40 @@
+<template>
+    <li>
+        <slot name="pre-icon">前置图标</slot>
+        <span v-if="!del" class="red">{{ title }}</span>
+        <span v-else style="text-decoration: line-through">{{ title }}</span>
+        <slot name="suf-icon">后置图标</slot>
+        <button v-if="!del" @click="handelClick">删除</button>
+        <slot name="last-icon" :value="value"></slot>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "TodoItem",
+        props: {
+            title: String,
+            del: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data() {
+            return {
+                value: Math.random()
+            }
+        },
+        methods: {
+            handelClick() {
+                console.log('点击了删除')
+                this.$emit('delete', this.title)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.red {
+    color: red;
+}
+</style>
